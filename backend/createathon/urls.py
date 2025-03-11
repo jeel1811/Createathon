ee19d2e9071d437ca7d8c8b4ee5a80af23e1ddf0
Jeel Hirani <134512076+jeel1811@users.noreply.github.com>
2025-03-11 14:29:44 +0530

    Update urls.py

--- backend/createathon/urls.py
+++ backend/createathon/urls.py
@@ -2,11 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework.authtoken.views import obtain_auth_token
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse('OK')
 
 urlpatterns = [
+    path('', health_check, name='health_check'),
     path('admin/', admin.site.urls),
-    path('api/token/', obtain_auth_token, name='api_token_auth'),
-    path('api/challenges/', include('challenges.urls')),
-    path('api/users/', include('users.urls')),
+    path('api/', include([
+        path('users/', include('users.urls')),
+        path('challenges/', include('challenges.urls')),
+    ])),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'createathon.views.custom_404'
